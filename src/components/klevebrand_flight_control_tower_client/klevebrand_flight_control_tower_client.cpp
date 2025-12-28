781eb0880d1f86ecb9020b1ef724c94dbdf8a3b2
Max Klevebrand <max.klevebrand@gmail.com>
2025-12-28 15:34:54 +0100

    Cleanup, fix full communication with flight control tower

--- src/components/klevebrand_flight_control_tower_client/klevebrand_flight_control_tower_client.cpp
+++ src/components/klevebrand_flight_control_tower_client/klevebrand_flight_control_tower_client.cpp
@@ -1,5 +1,4 @@
 #include "klevebrand_flight_control_tower_client.h"
-#include "ArduinoJson.h"
 
 void KlevebrandFlightControlTowerClient::setup()
 {
@@ -11,46 +10,15 @@ DroneRequestDto KlevebrandFlightControlTowerClient::getDroneRequest(String seria
     String path = "api/v1/dronerequest/" + serial_number;
     SkywireResponseResult_t result = http_stepper_client.get(path);
 
-    int response_json_start = result.response_content.indexOf('{');
-    int response_json_end = result.response_content.lastIndexOf('}');
-
-    if (result.is_success && response_json_start != -1 && response_json_end != -1)
+    if (result.is_success)
     {
-        String response_json = result.response_content.substring(response_json_start, response_json_end + 1);
+        Serial.println("Received response from flight control tower:");
+        Serial.println(result.response_content);
 
-        JsonDocument doc;
-        deserializeJson(doc, response_json);
+        DroneRequestDto drone_request = DroneRequestDto::parseFromCsvString(result.response_content);
 
-        Serial.println("Drone request JSON, json start: " + String(response_json_start) + ", json end: " + String(response_json_end));
-        Serial.println(response_json);
-
-        int flight_mode_id = doc["flight_mode_id"] | 0;
-        float throttle = doc["throttle"] | 0.0f;
-        float yaw = doc["yaw"] | 0.0f;
-        float pitch = doc["pitch"] | 0.0f;
-        float roll = doc["roll"] | 0.0f;
-        bool enable_power = doc["enable_power"] | false;
-        bool enable_motors = doc["enable_motors"] | false;
-
-        return DroneRequestDto(
-            flight_mode_id,
-            throttle,
-            yaw,
-            pitch,
-            roll,
-            enable_power,
-            enable_motors,
-            true);
+        return drone_request;
     }
 
-    return DroneRequestDto(
-        0,
-        0.0f,
-        0.0f,
-        0.0f,
-        0.0f,
-        false,
-        false,
-        false
-    );
+    return DroneRequestDto::getInvalidDroneRequest();
 }
