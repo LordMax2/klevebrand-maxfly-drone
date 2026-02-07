fb3e0a7069fe2d71ac797ebbe00ed34c1ab23318
Max Klevebrand <max.klevebrand@gmail.com>
2026-02-08 00:19:09 +0100

    Reorder steps to do non blocking first Upgrade to version with stepper error handling and timeout handling

--- include/klevebrand_skywire_http_gps_step_worker.h
+++ include/klevebrand_skywire_http_gps_step_worker.h
@@ -24,15 +24,15 @@ public:
 		void (*on_http_completed_function)(String result_content) = nullptr,
 		void (*on_gps_completed_function)(String response_content) = nullptr,
 		unsigned long timeout_milliseconds = 5000,
-		bool debug_mode = false) : SkywireStepWorker(skywire_serial, debug_mode, STEP_COUNT)
+		bool debug_mode = false) : SkywireStepWorker(skywire_serial, debug_mode, timeout_milliseconds, STEP_COUNT)
 	{
 		this->steps = new SkywireStep *[STEP_COUNT];
 
-		this->steps[0] = new SkywireStep(skywire, "AT#HTTPCFG=0,\"" + base_url + "\"", debug_mode, nullptr);
-		this->steps[1] = new SkywireStep(skywire, "AT#HTTPQRY=0,0,\"/" + path + "\"", debug_mode, nullptr);
-		this->steps[2] = new HttpRingSkywireStep(skywire, debug_mode, nullptr);
-		this->steps[3] = new HttpRcvSkywireStep(skywire, debug_mode, setLatestHttpResponse);
-		this->steps[4] = new GpsAcpSkywireStep(skywire, debug_mode, setLatestGpsResponse);
+		this->steps[0] = new GpsAcpSkywireStep(skywire, debug_mode, setLatestGpsResponse);
+		this->steps[1] = new SkywireStep(skywire, "AT#HTTPCFG=0,\"" + base_url + "\",80,0,\"\",\"\",0,5", debug_mode, nullptr);
+		this->steps[2] = new SkywireStep(skywire, "AT#HTTPQRY=0,0,\"/" + path + "\"", debug_mode, nullptr);
+		this->steps[3] = new HttpRingSkywireStep(skywire, debug_mode, nullptr);
+		this->steps[4] = new HttpRcvSkywireStep(skywire, debug_mode, setLatestHttpResponse);
 
 		resetState();
 	}
