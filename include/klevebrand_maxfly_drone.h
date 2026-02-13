7e22285eb2706e7c2f973cc4b9e00d372ae097f0
Max Klevebrand <max.klevebrand@gmail.com>
2026-02-13 18:13:47 +0100

    Move arduino processor interface to new file

--- include/klevebrand_maxfly_drone.h
+++ include/klevebrand_maxfly_drone.h
@@ -7,45 +7,7 @@
 #include "quadcopter_pid.h"
 #include "bno08x_drone_gyro.h"
 #include "eeprom_pid_repository.h"
-
-class HardwareProcessorArduino : public BaseHardwareProcessor
-{
-public:
-  HardwareProcessorArduino() : BaseHardwareProcessor() {};
-
-  void setup() override
-  {
-    Serial.begin(115200);
-
-    if (!Serial)
-    {
-        Serial.println("FAILED TO START SERIAL...");
-    }
-    while (!Serial)
-    {
-    }
-  }
-
-  unsigned long microsecondsTimestamp() override
-  {
-    return micros();
-  }
-
-  unsigned long millisecondsTimestamp() override
-  {
-    return millis();
-  }
-
-  void sleepMilliseconds(int milliseconds) override
-  {
-    delay(milliseconds);
-  }
-
-  void print(char *array) override
-  {
-    Serial.println(array);
-  }
-};
+#include "hardware_processor_arduino.h"
 
 class KlevebrandMaxFlyDrone : public TemplateGyroDrone<QuadcopterPid>
 {
