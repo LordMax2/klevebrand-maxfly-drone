0cec17e80fcc4cfd7844c642d8f0cbe33f682182
LordMax2 <max.klevebrand@gmail.com>
2026-05-01 22:34:48 +0200

    Test new PID constants

--- include/flight_mode_acro_local.h
+++ include/flight_mode_acro_local.h
@@ -18,9 +18,9 @@ public:
     PidConstants_t pidConstants() const override
     {
         return {
-            0.040f, 0.030f, 0.003f,
-            0.040f, 0.050f, 0.003f,
-            0.040f, 0.050f, 0.003f
+            0.04f, 0.002f / 0.005f, 0.6f * 0.005f,
+            0.04f, 0.002f / 0.005f, 0.6f * 0.005f,
+            0.04f, 0.002f / 0.005f, 0.6f * 0.005f
         };
     }
 };
